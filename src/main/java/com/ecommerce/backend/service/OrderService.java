05d5ed14e7a8c9e987df6f52154cdc042d5d6538
Lestro~ <87465873+Lestroboy@users.noreply.github.com>
2026-01-10 20:10:08 +0530

    Delete src directory

--- src/main/java/com/ecommerce/backend/service/OrderService.java
+++
@@ -1,8 +0,0 @@
-package com.ecommerce.backend.service;
-
-import com.ecommerce.backend.dto.CheckoutRequestDTO;
-import com.ecommerce.backend.entity.Order;
-
-public interface OrderService {
-    Order placeOrder(CheckoutRequestDTO checkoutRequest);
-}
